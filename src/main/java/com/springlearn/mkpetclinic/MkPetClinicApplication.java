dfb63f34982d06a4a205d4649d24c8e53d0b03e5
Manoj Kumar <manoj.k0508@gmail.com>
2021-07-15 15:20:25 +0530

    new project created with all dependecy

---
+++ src/main/java/com/springlearn/mkpetclinic/MkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springlearn.mkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkPetClinicApplication.class, args);
+    }
+
+}
