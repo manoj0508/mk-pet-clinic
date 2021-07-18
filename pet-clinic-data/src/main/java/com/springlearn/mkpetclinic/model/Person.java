91511c59a5ee58c940967edf7f859b2c1befde75
Manoj Kumar <manoj.k0508@gmail.com>
2021-07-18 20:17:47 +0530

    Added Base entity for id parameters

--- pet-clinic-data/src/main/java/com/springlearn/mkpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/springlearn/mkpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.springlearn.mkpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
 
--- pet-clinic-data/src/main/java/com/springlearn/mkpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/springlearn/mkpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.springlearn.mkpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
